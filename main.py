a4e3a004d9d66d5a8777139f17b5315f6fc655d8
Unknown <hpf.vanheijst@gmail.com>
2018-12-03 22:26:10 +0100

    Created example flow with exercises

--- main.py
+++ main.py
@@ -18,17 +18,53 @@ def index():
 def opdracht_1():
     # Check if valid answer
     if request.method == 'POST':
-        if request.form['answer'] != '5':
+        if request.form['answer'] != '-9':
             error = 'Fout antwoord! Probeer opnieuw.'
         else:
             return redirect(url_for('opdracht_2'))
         return render_template('opdracht_1.html', error=error)
-            
+
     return  render_template('opdracht_1.html')
 
 @app.route('/opdracht2/')
 def opdracht_2():
     return render_template('opdracht_2.html')
+
+@app.route('/opdracht2a/', methods=['GET', 'POST'])
+def opdracht_2a():
+    # Check if valid answer
+    if request.method == 'POST':
+        if request.form['answer2'].lower() != 'madrid':
+            error = 'Fout antwoord! Probeer opnieuw.'
+        else:
+            return redirect(url_for('gapminder'))
+        return render_template('opdracht_2a.html', error=error)
+
+    return render_template('opdracht_2a.html')
+
+@app.route('/gapminder/')
+def gapminder():
+    return render_template('gapminder.html')
+
+@app.route('/opdracht3/', methods=['GET', 'POST'])
+def opdracht_3():
+    # Check if valid answer
+    if request.method == 'POST':
+        if request.form['answer3'].lower() != 'japan':
+            error = 'Fout antwoord! Probeer opnieuw.'
+        else:
+            return redirect(url_for('finish'))
+        return render_template('opdracht_3.html', error=error)
+
+    return render_template('opdracht_3.html')
+
+@app.route('/finish/')
+def finish():
+    return render_template('finish_123hidde.html')
+
+@app.route('/cadeau/')
+def cadeau():
+    return render_template('cadeau.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
